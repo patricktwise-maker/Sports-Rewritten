63880fb5385b8349c5c3dd29b35f07efdc6082ac
patricktwise-maker <patricktwise@gmail.com>
2026-09-16 09:33:51 -0400

    Add prototype content data

---
+++ src/lib/content.ts
@@ -0,0 +1,10 @@
+export const articles = [
+  { title: "What If Joe Hamilton Was Born 20 Years Later?", sport: "College Football", read: "22 min", slug: "/college-football/what-if-joe-hamilton-was-born-20-years-later" },
+  { title: "What If Kobe Bryant Had Gone to Duke?", sport: "College Basketball", read: "15 min", slug: "#" },
+  { title: "What If Len Bias Had Lived?", sport: "NBA", read: "17 min", slug: "#" },
+  { title: "What If Derrick Rose Never Tore His ACL?", sport: "NBA", read: "16 min", slug: "#" },
+  { title: "What If Randy Moss Had Been Drafted by Dallas?", sport: "NFL", read: "14 min", slug: "#" },
+  { title: "What If the Chris Paul Trade to the Lakers Had Gone Through?", sport: "NBA", read: "18 min", slug: "#" },
+];
+
+export const sports = ["NFL", "NBA", "College Football", "College Basketball", "MLB", "Boxing", "Other Sports"];
